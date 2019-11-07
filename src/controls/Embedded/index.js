e491b234e5297478fc7f961a5060ab248aa2527d
Shyjal <shyjal99@gmail.com>
2019-11-07 22:16:10 +0530

    First

---
+++ src/controls/Embedded/index.js
@@ -0,0 +1,104 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AtomicBlockUtils } from 'draft-js';
+
+import LayoutComponent from './Component';
+
+class Embedded extends Component {
+  static propTypes: Object = {
+    editorState: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+    modalHandler: PropTypes.object,
+    config: PropTypes.object,
+    translations: PropTypes.object,
+  };
+
+  state: Object = {
+    expanded: false,
+  };
+
+  componentWillMount(): void {
+    const { modalHandler } = this.props;
+    modalHandler.registerCallBack(this.expandCollapse);
+  }
+
+  componentWillUnmount(): void {
+    const { modalHandler } = this.props;
+    modalHandler.deregisterCallBack(this.expandCollapse);
+  }
+
+  onExpandEvent: Function = (): void => {
+    this.signalExpanded = !this.state.expanded;
+  };
+
+  expandCollapse: Function = (): void => {
+    this.setState({
+      expanded: this.signalExpanded,
+    });
+    this.signalExpanded = false;
+  }
+
+  doExpand: Function = (): void => {
+    this.setState({
+      expanded: true,
+    });
+  };
+
+  doCollapse: Function = (): void => {
+    this.setState({
+      expanded: false,
+    });
+  };
+
+  addEmbeddedLink: Function = (embeddedLink, height, width): void => {
+
+
+    if (embeddedLink.indexOf("youtube") >= 0) {
+      embeddedLink = embeddedLink.replace("watch?v=","embed/");
+      embeddedLink = embeddedLink.replace("/watch/", "/embed/");
+      embeddedLink = embeddedLink.replace("youtu.be/","youtube.com/embed/");
+    } else if(embeddedLink.indexOf("vimeo") >= 0) {
+      embeddedLink = embeddedLink.replace("vimeo.com/","player.vimeo.com/video/");
+    } else if(embeddedLink.indexOf("facebook") >= 0) {
+      embeddedLink = embeddedLink.replace("https://www.facebook.com","https://www.facebook.com/plugins/video.php?href=https://www.facebook.com");
+    } else if(embeddedLink.indexOf("instagram") >= 0) {
+      embeddedLink = embeddedLink.split('?')[0].replace(/\/$/, "") + '/embed';
+    }
+    
+    const { editorState, onChange } = this.props;
+    const entityKey = editorState
+      .getCurrentContent()
+      .createEntity('EMBEDDED_LINK', 'MUTABLE', { src: embeddedLink, height, width })
+      .getLastCreatedEntityKey();
+    const newEditorState = AtomicBlockUtils.insertAtomicBlock(
+      editorState,
+      entityKey,
+      ' ',
+    );
+    onChange(newEditorState);
+    this.doCollapse();
+  };
+
+  render(): Object {
+    const { config, translations } = this.props;
+    const { expanded } = this.state;
+    const EmbeddedComponent = config.component || LayoutComponent;
+    return (
+      <EmbeddedComponent
+        config={config}
+        translations={translations}
+        onChange={this.addEmbeddedLink}
+        expanded={expanded}
+        onExpandEvent={this.onExpandEvent}
+        doExpand={this.doExpand}
+        doCollapse={this.doCollapse}
+      />
+    );
+  }
+}
+
+export default Embedded;
+
+// todo: make default heights configurable
